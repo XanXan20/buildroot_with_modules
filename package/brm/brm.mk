cc8dbfa8dff55a238faa9cfbbbad705d96e0827a
Egor Krasilnikov <e.krasilnikov.work@gmail.com>
2024-05-08 10:58:15 +0300

    adding the brm package

---
+++ package/brm/brm.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# brm
+#
+################################################################################
+
+BRM_VERSION = v0.1-alpha
+BRM_SITE = package/brm/src
+BRM_SITE_METHOD = local
+
+define BRM_INSTALL_TARGET_CMDS
+	cp $(@D)/preinit $(TARGET_DIR)/sbin
+	mkdir -p $(TARGET_DIR)/sbin/brm
+	install $(@D)/brm $(TARGET_DIR)/sbin/brm
+	cp $(@D)/.path $(TARGET_DIR)/sbin/brm
+endef
+
+$(eval $(generic-package))
